01363a1fed64aec35bc038a6fc5a139641019d19
hWataame <151874689+HWataame@users.noreply.github.com>
2025-07-23 13:27:19 +0900

    feat: アセンブリ情報を追加

---
+++ Scripts/AssemblyInfo/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+/*
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+アセンブリ情報を保持するファイル
+
+AssemblyInfo.cs
+────────────────────────────────────────
+バージョン: 1.0.1
+2025 Wataame(HWataame)
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+*/
+using System.Reflection;
+
+[assembly: AssemblyTitle("Binary Selector")]
+[assembly: AssemblyProduct("Binary Selector")]
+[assembly: AssemblyCompany("Wataame(HWataame)")]
+[assembly: AssemblyCopyright("2025 Wataame(HWataame)")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
